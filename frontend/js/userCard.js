21040aed825c0124757a80bc6d8088e8f63ec8e1
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-02 22:22:42 +0300

    building user card frontend

---
+++ frontend/js/userCard.js
@@ -0,0 +1,18 @@
+const userCard = () => {
+  return `
+    <div class='user-card'>
+        <div class='card-img'>
+            <img src='./assets/mock-img-1.png' alt=""/>
+        </div>
+        <div class='card-body'>
+            <div class='card-text'>
+                <div class='card-title'>Full Name</div>
+                <div>Status: <span class='green'>online</span></div>
+                <div>Distance: 3.2km</div>
+            </div>
+            <div class='card-like'>
+                <img src='./assets/unactive-heart-icon.svg'
+            </div>
+        </div>
+    </div>`;
+};
