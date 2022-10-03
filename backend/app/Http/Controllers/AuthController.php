9cd6c7df3bd50b7b19b029f4293132d2bf50d1ed
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-04 02:33:21 +0300

    configuring jwt backend

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -5,43 +5,48 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 
-class AuthController extends Controller
-{
+class AuthController extends Controller{
    
-    public function signIn()
-    {
+    public function signIn(){
         $credentials = request(['email', 'password']);
 
         if (! $token = auth()->attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
-
         return $this->respondWithToken($token);
     }
 
+    function signUp(Request $request){
+      $user=new User();
+      $user->email=$request['email'];
+      $user->name=$request['name'];
+      $user->password=$request['password'];
+      $user->save();
+      
+      return $this->respondWithToken($user->token);
+
+    }
+
    
-    public function me()
-    {
+    public function me(){
         return response()->json(auth()->user());
     }
 
-    public function logout()
-    {
+    public function logout(){
         auth()->logout();
 
         return response()->json(['message' => 'Successfully logged out']);
     }
 
     
-    public function refresh()
-    {
+    public function refresh(){
         return $this->respondWithToken(auth()->refresh());
     }
 
    
-    protected function respondWithToken($token)
-    {
+    protected function respondWithToken($token){
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
