d0f0e1f9e52e4e0bcfdf184c97ad7d098eb736b6
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-03 20:38:54 +0300

    adding jwt to the project backend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,40 +1,27 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SignController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\MessagesController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::group(["prefix"=> "v0.1"], function(){
 
-    Route::post("/sign/signin", [SignController::class, 'signIn']);
-    Route::post("/sign/signup", [SignController::class, 'signUp']);
-    Route::post("/users/update_user/{id}", [UsersController::class, 'updateUser']);
-    Route::post("/users/block/{id}", [UsersController::class, 'switchBlock']);
-    Route::get("/users/all/{id}", [UsersController::class, 'fetchAllUsers']);
-    Route::get("/users/contact/all/{id}", [UsersController::class, 'fetchAllContact']);
-    Route::post("/users/like/{id}", [UsersController::class, 'switchLike']);
-    Route::post("/messages/send", [MessagesController::class, 'addingMessage']);
-    Route::get("/messages/all/{id}", [MessagesController::class, 'addingMessage']);
-
+    Route::get("/sign/signin", [AuthController::class, 'signIn'])->name('login');
+    Route::post("/sign/signin", [AuthController::class, 'signIn']);
+    Route::post("/sign/signup", [AuthController::class, 'signUp']);
+    
+    Route::group(['middleware'=> 'auth:api'], function(){
+        Route::get("/users/all/{id}", [UsersController::class, 'fetchAllUsers']);
+        Route::post("/users/update_user/{id}", [UsersController::class, 'updateUser']);
+        Route::post("/users/block/{id}", [UsersController::class, 'switchBlock']);
+        Route::get("/users/contact/all/{id}", [UsersController::class, 'fetchAllContact']);
+        Route::post("/users/like/{id}", [UsersController::class, 'switchLike']);
+        Route::post("/messages/send", [MessagesController::class, 'addingMessage']);
+        Route::get("/messages/all/{id}", [MessagesController::class, 'addingMessage']);
+    });
 
     Route::get("/not_found", [LandingController::class, "notFound"])->name("not-found");
 
-});
-
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
 });
