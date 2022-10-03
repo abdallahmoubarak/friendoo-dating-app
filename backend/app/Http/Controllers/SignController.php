e6b95d4c9c2bd3475f4343345ae91b2cb366abfe
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-03 14:36:38 +0300

    connecting routes to controller backend

--- backend/app/Http/Controllers/SignController.php
+++ backend/app/Http/Controllers/SignController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class SignController extends Controller{
 
+    function signIn(Request $request){
+        $name =  $request->name;
+        return 'hi '.$name;
+    }
+
 
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SignController;
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\MessagesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,14 +20,14 @@ use Illuminate\Support\Facades\Route;
 Route::group(["prefix"=> "v0.1"], function(){
 
     Route::post("/sign/signin", [SignController::class, 'signIn']);
-    // Route::post("/sign/signup", [SignController::class, 'signUp']);
-    // Route::post("/users/update_user/{id}", [UsersController::class, 'updateUser']);
-    // Route::post("/users/block/{id}", [UsersController::class, 'switchBlock']);
-    // Route::get("/users/all/{id}", [UsersController::class, 'fetchAllUsers']);
-    // Route::get("/users/contact/all/{id}", [UsersController::class, 'fetchAllContact']);
-    // Route::post("/users/like/{id}", [UsersController::class, 'switchLike']);
-    // Route::post("/messages/send", [MessagesController::class, 'addingMessage']);
-    // Route::get("/messages/all/{id}", [MessagesController::class, 'addingMessage']);
+    Route::post("/sign/signup", [SignController::class, 'signUp']);
+    Route::post("/users/update_user/{id}", [UsersController::class, 'updateUser']);
+    Route::post("/users/block/{id}", [UsersController::class, 'switchBlock']);
+    Route::get("/users/all/{id}", [UsersController::class, 'fetchAllUsers']);
+    Route::get("/users/contact/all/{id}", [UsersController::class, 'fetchAllContact']);
+    Route::post("/users/like/{id}", [UsersController::class, 'switchLike']);
+    Route::post("/messages/send", [MessagesController::class, 'addingMessage']);
+    Route::get("/messages/all/{id}", [MessagesController::class, 'addingMessage']);
 
 
     Route::get("/not_found", [LandingController::class, "notFound"])->name("not-found");
