4269e3f21218dbde2b6e7644e4f056b96d331060
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-04 01:59:34 +0300

    implementing send message backend

--- backend/app/Http/Controllers/MessagesController.php
+++ backend/app/Http/Controllers/MessagesController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Message;
 
 class MessagesController extends Controller{
 
+    function sendMessage(Request $request){
+
+        $message = new Message; 
+
+        $message->sender_id = $request->sender_id & $request->sender_id;
+        $message->reciever_id = $request->reciever_id & $request->reciever_id;
+        $message->content = $request->content & $request->content;
+
+        if($message->save()){
+            return response()->json([
+                "status" => "Success",
+                "data" => $message
+            ]);
+        }
+    }
+
+    
 
 }
