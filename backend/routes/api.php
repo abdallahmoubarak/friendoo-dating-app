3b81a0ef6cbb5e1ddce903d1f110f1fc4c474171
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-03 14:17:01 +0300

    rebuilding the Laravel backend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,14 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::group(["prefix"=> "v0.1"], function(){
 
     Route::post("/sign/signin", [SignController::class, 'signIn']);
-    Route::post("/sign/signup", [SignController::class, 'signUp']);
-    Route::post("/users/update_user/{id}", [UsersController::class, 'updateUser']);
-    Route::post("/users/block/{id}", [UsersController::class, 'switchBlock']);
-    Route::get("/users/all/{id}", [UsersController::class, 'fetchAllUsers']);
-    Route::get("/users/contact/all/{id}", [UsersController::class, 'fetchAllContact']);
-    Route::post("/users/like/{id}", [UsersController::class, 'switchLike']);
-    Route::post("/messages/send", [MessagesController::class, 'addingMessage']);
-    Route::get("/messages/all/{id}", [MessagesController::class, 'addingMessage']);
+    // Route::post("/sign/signup", [SignController::class, 'signUp']);
+    // Route::post("/users/update_user/{id}", [UsersController::class, 'updateUser']);
+    // Route::post("/users/block/{id}", [UsersController::class, 'switchBlock']);
+    // Route::get("/users/all/{id}", [UsersController::class, 'fetchAllUsers']);
+    // Route::get("/users/contact/all/{id}", [UsersController::class, 'fetchAllContact']);
+    // Route::post("/users/like/{id}", [UsersController::class, 'switchLike']);
+    // Route::post("/messages/send", [MessagesController::class, 'addingMessage']);
+    // Route::get("/messages/all/{id}", [MessagesController::class, 'addingMessage']);
 
 
     Route::get("/not_found", [LandingController::class, "notFound"])->name("not-found");
