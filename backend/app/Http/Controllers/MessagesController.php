4aed1c5c692c5b557db6d948f2bf4be620cfc3ea
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-04 02:30:34 +0300

    implementing get messages backend

--- backend/app/Http/Controllers/MessagesController.php
+++ backend/app/Http/Controllers/MessagesController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Message;
+use PhpParser\Node\Expr\AssignOp\Concat;
+
+use function Illuminate\Validation\Rules\message;
 
 class MessagesController extends Controller{
 
@@ -21,8 +24,22 @@ class MessagesController extends Controller{
                 "data" => $message
             ]);
         }
+        return response()->json(["status" => "Error"]);
     }
 
-    
+    function getMessages($sender_id,$reciever_id){
+        
+        $messages1 = Message::where('sender_id', $sender_id)
+            ->where('reciever_id', $reciever_id)
+            ->get();
+        
+        $messages2 = Message::where('sender_id', $reciever_id)
+            ->where('reciever_id', $sender_id)
+            ->get();
+        
+        $messages = $messages1->merge($messages2);
+
+        return response()->json($messages->all());
+    }
 
 }
