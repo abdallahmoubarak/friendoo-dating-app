d5d6ef3f2d78969ddcd4122b7c2f17a7a3228752
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-04 02:33:03 +0300

    fixing messages route backend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -9,18 +9,18 @@ use App\Http\Controllers\MessagesController;
 Route::group(["prefix"=> "v0.1"], function(){
 
     Route::get("/sign/signin", [AuthController::class, 'signIn'])->name('login');
-    Route::post("/sign/signin", [AuthController::class, 'signIn']);
+    Route::post("/sign/signin", [AuthController::class, 'signIn'])->name('login');
     Route::post("/sign/signup", [AuthController::class, 'signUp']);
     
-    Route::group(['middleware'=> 'auth:api'], function(){
-        Route::get("/users/all/{id}", [UsersController::class, 'fetchAllUsers']);
-        Route::post("/users/update_user/{id}", [UsersController::class, 'updateUser']);
-        Route::post("/users/block/{id}", [UsersController::class, 'switchBlock']);
-        Route::get("/users/contact/all/{id}", [UsersController::class, 'fetchAllContact']);
-        Route::post("/users/like/{id}", [UsersController::class, 'switchLike']);
-        Route::post("/messages/send", [MessagesController::class, 'addingMessage']);
-        Route::get("/messages/all/{id}", [MessagesController::class, 'addingMessage']);
-    });
+    Route::get("/users/all/{id?}", [UsersController::class, 'fetchAllUsers']);
+    Route::post("/users/update_user/{id?}", [UsersController::class, 'updateUser']);
+    Route::post("/users/block/{id?}", [UsersController::class, 'switchBlock']);
+    Route::get("/users/contact/all/{id?}", [UsersController::class, 'fetchAllContact']);
+    Route::post("/users/like/{id?}", [UsersController::class, 'switchLike']);
+    Route::post("/messages/send", [MessagesController::class, 'sendMessage']);
+    Route::get("/messages/all/{sender_id?}/{reciever_id?}", [MessagesController::class, 'getMessages']);
+    // Route::group(['middleware'=> 'auth:api'], function(){
+    // });
 
     Route::get("/not_found", [LandingController::class, "notFound"])->name("not-found");
 
