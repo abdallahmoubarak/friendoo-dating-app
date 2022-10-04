efedbefb4b5c6e748d1aafc7a4a8393b8395f64d
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-04 23:39:46 +0300

    repairing helpers function frontend

--- frontend/js/helpers.js
+++ frontend/js/helpers.js
@@ -2,9 +2,13 @@ const friendoo = {};
 
 friendoo.baseURL = "http://127.0.0.1:8000/api/v0.1";
 
-friendoo.getAPI = async (api_route) => {
+friendoo.getAPI = async (api_route, api_token = null) => {
   try {
-    const res = await axios(friendoo.baseURL + api_route);
+    const res = await axios(friendoo.baseURL + api_route, {
+      headers: {
+        Authorization: "Bearer " + api_token,
+      },
+    });
     return res;
   } catch (error) {
     return error.response.data.message;
@@ -15,7 +19,7 @@ friendoo.postAPI = async (api_route, api_data, api_token = null) => {
   try {
     const res = await axios.post(friendoo.baseURL + api_route, api_data, {
       headers: {
-        Authorization: "token " + api_token,
+        Authorization: "Bearer " + api_token,
       },
     });
     return res;
