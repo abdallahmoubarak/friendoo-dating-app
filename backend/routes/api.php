f1d56cf86950d9305e07369f6ea33fb5b1d353a9
Abdallah <abdallah.moubarak02@gmail.com>
2022-10-04 16:08:32 +0300

    implementing jwt in different method frontend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,17 +1,18 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\MessagesController;
 
-
 Route::group(["prefix"=> "v0.1"], function(){
+      
+    Route::controller(AuthController::class)->group(function () {
+        Route::post("/sign/signin", 'login');
+        Route::post("/sign/signup", 'register');
+    });
 
-    Route::get("/sign/signin", [AuthController::class, 'signIn'])->name('login');
-    Route::post("/sign/signin", [AuthController::class, 'signIn'])->name('login');
-    Route::post("/sign/signup", [AuthController::class, 'signUp']);
-    
     Route::post("/users/update_user/{id?}", [UsersController::class, 'updateUser']);
     Route::get("/users/all/{id?}", [UsersController::class, 'getInterested']);
     Route::get("/users/contact/all/{id?}", [UsersController::class, 'getContact']);
@@ -22,6 +23,5 @@ Route::group(["prefix"=> "v0.1"], function(){
     // Route::group(['middleware'=> 'auth:api'], function(){
     // });
 
-    Route::get("/not_found", [LandingController::class, "notFound"])->name("not-found");
 
 });
